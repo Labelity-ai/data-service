9cc5e312aedf4db9b634a7b629ed638e5732cbb8
Antonio Molner <antoniomdk@hotmail.es>
2021-02-09 10:36:05 +0100

    Add bulk post endpoint

--- app/models.py
+++ app/models.py
@@ -1,6 +1,14 @@
 from typing import Optional, Dict, Any, Tuple, Union, List
 from odmantic import Model, ObjectId, EmbeddedModel
 from app.utils import json_loads, json_dumps
+from pydantic import validator
+
+
+def check_relative_points(values: List[float]):
+    for value in values:
+        if value > 1.0:
+            raise ValueError('Coordinates should be relative (between 0 and 1)')
+    return values
 
 
 class Prediction(EmbeddedModel):
@@ -23,20 +31,40 @@ class Tag(Prediction):
 class Detection(Prediction):
     box: Tuple[float, float, float, float]
 
+    @validator('box')
+    def check_box(cls, box):
+        check_relative_points(box)
+        return box
+
 
 class Keypoint(Prediction):
     point: Tuple[float, float]
     label: str
+
+    @validator('point')
+    def check_points(cls, point):
+        check_relative_points(point)
+        return point
 
 
 class Polygon(Prediction):
     points: List[Tuple[float, float]]
     label: str
 
+    @validator('points')
+    def check_points(cls, points):
+        check_relative_points(points)
+        return points
+
 
 class Polyline(Prediction):
     points: List[Tuple[float, float]]
     label: str
+
+    @validator('points')
+    def check_points(cls, points):
+        check_relative_points(points)
+        return points
 
 
 class ImageAnnotations(Model):
