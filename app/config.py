90637d58c6206cfb6a8551755dd0c5b661e76967
Antonio Molner <antoniomdk@hotmail.es>
2021-02-10 18:41:12 +0100

    Add some views and the rest of models

--- app/config.py
+++ app/config.py
@@ -3,3 +3,5 @@ import os
 
 class Config:
     POST_BULK_LIMIT = os.environ.get('POST_BULK_LIMIT', 1000)
+    MONGO_HOST = os.environ.get('MONGO_URI', 'mongodb://localhost:27017')
+    MONGO_DATABASE = os.environ.get('MONGO_URI', 'default_db')
--- app/schema.py
+++ app/schema.py
@@ -1,14 +1,19 @@
 from typing import List
+from functools import partial
+from enum import Enum
 from pydantic import BaseModel
-from models import Polyline, Detection, Polygon, Tag
+from fastapi_utils.camelcase import snake2camel
+
+from app.models import Polyline, Detection, Polygon, Tag
 from app.utils import json_dumps, json_loads
 
 
 class SchemaBase(BaseModel):
-
     class Config:
         json_loads = json_loads
         json_dumps = json_dumps
+        allow_population_by_field_name = True
+        alias_generator = partial(snake2camel, start_lower=True)
 
 
 class ImageAnnotationsPostSchema(SchemaBase):
@@ -25,3 +30,7 @@ class ImageAnnotationsPostSchema(SchemaBase):
         json_dumps = json_dumps
 
 
+class AnnotationsFormat(Enum):
+    CVAT_XML = 'CVAT_XML'
+    COCO = 'COCO'
+    LABELME = 'LABELME_3.0'
