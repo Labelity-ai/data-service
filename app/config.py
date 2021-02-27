0693b6f7e170c140300729978446c28fd1302c93
Antonio Molner <antoniomdk@hotmail.es>
2021-02-27 15:14:46 +0100

    Add initial version of video to frame conversor

--- app/config.py
+++ app/config.py
@@ -11,7 +11,9 @@ class Config:
     MONGO_DATABASE = os.environ.get('MONGO_DATABASE', 'default_database')
     IMAGE_STORAGE_BUCKET = os.environ.get('IMAGE_STORAGE_BUCKET', 'labelity-use1-dev-user-storage')
     RAW_IMAGES_FOLDER = os.environ.get('RAW_IMAGES_FOLDER', 'raw')
+    VIDEOS_FOLDER = os.environ.get('VIDEOS_FOLDER', 'videos')
     THUMBNAILS_FOLDER = os.environ.get('THUMBNAILS_FOLDER', 'thumbnails')
     OPTIMIZED_IMAGES_FOLDER = os.environ.get('OPTIMIZED_IMAGES_FOLDER', 'optimized')
     SIGNED_POST_URL_EXPIRATION = os.environ.get('SIGNED_POST_URL_EXPIRATION', 100)
     SECRET_KEY = os.environ.get('SECRET_KEY', 'b308c184a53819197ae49b274ae10dcf0a590ee2659925fd440c918ce30f96ea')
+    VIDEO_FPS_LIMIT = 5
